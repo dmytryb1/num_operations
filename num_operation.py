6675b07386e75415f2f56b61d6972aebc7a7c31d
Dmytry <dmytry.lb1@gmail.com>
2023-10-21 04:46:05 +0500

    Main code
    Main python code file

---
+++ num_operation.py
@@ -0,0 +1,11 @@
+num = 12345
+numlist = list(str(num))
+print(numlist)
+
+a = float(numlist[0])
+b = float(numlist[1])
+c = float(numlist[2])
+d = float(numlist[3])
+e = float(numlist[4])
+
+print(((d ** e) * c) / (a - b) )
